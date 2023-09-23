e1a9403dbdc6001647403ca7565be40da9706612
ammar_ib <ammaryasir8088@gmail.com>
2023-09-23 14:13:25 +0300

    updates

--- app.js
+++ app.js
@@ -1,32 +1,14 @@
-// to controll website
-
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 const helmet = require("helmet");
 
-app.set("view engine", "ejs");
-app.use(express.static("public"));
-app.use(express.urlencoded({ extended: true }));
-const Contact = require("./models/ContactSchema");
-const User = require("./models/UserSchema");
-const Posts = require("./models/articleSchema");
 const mongoose = require("mongoose");
 const cookieParser = require("cookie-parser");
 const session = require("express-session");
 const nodemailer = require("nodemailer");
 
-app.use(cookieParser());
-app.use(
-  session({
-    secret: "secret key",
-    resave: false,
-    saveUninitialized: false,
-    cookie: { secure: false },
-  })
-);
 
-// for auto refresh
 const path = require("path");
 const livereload = require("livereload");
 const liveReloadServer = livereload.createServer();
@@ -42,28 +24,47 @@ liveReloadServer.server.once("connection", () => {
 });
 
 // mongoose
-
 mongoose
-  .connect(
-    "mongodb+srv://ammar:alibrahim@cluster0.51i7rk6.mongodb.net/?retryWrites=true&w=majority"
-  )
-  .then((result) => {
-    app.listen(process.env.PORT || port, () => {
+  .connect("mongodb+srv://ammar:alibrahim@cluster0.51i7rk6.mongodb.net/your-database-name", {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+  })
+  .then(() => {
+    app.listen(port, () => {
       console.log(`Example app listening at http://localhost:${port}`);
     });
   })
-
   .catch((err) => {
     console.log(err);
   });
 
 app.use(helmet());
 
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(express.urlencoded({ extended: true }));
+
+// Define your Mongoose models and schemas here.
+const Contact = require("./models/ContactSchema");
+const User = require("./models/UserSchema");
+const Posts = require("./models/articleSchema");
+
+app.use(cookieParser());
+app.use(
+  session({
+    secret: "secret key",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { secure: false },
+  })
+);
+
 app.get("/", (req, res) => {
   res.redirect("/home");
 });
 
 app.get("/home", (req, res) => {
+  // Fetch and render articles from your database (Posts model).
   Posts.find()
     .then((result) => {
       res.render("home", { arrArticle: result });
@@ -73,20 +74,11 @@ app.get("/home", (req, res) => {
     });
 });
 
-app.get("/create-post", (req, res) => {
-  res.render("create-post", { mytitle: "create new article" });
-});
-
 app.post("/home", (req, res) => {
+  // Handle the creation and saving of new articles.
   const { title, summary, number, shoesname, body } = req.body;
+  const article = new Posts({ title, summary, number, shoesname, body });
 
-  const contact = new Contact({ title, summary, number, shoesname, body });
-
-  const article = new Posts(req.body);
-
-  // veri nesnesi oluşturma
-
-  // veri nesnesinin veritabanına kaydedilmesi
   article
     .save()
     .then((result) => {
@@ -97,17 +89,6 @@ app.post("/home", (req, res) => {
     });
 });
 
-app.get("/all-shoes", (req, res) => {
-  Contact.find()
-    .then((articles) => {
-      res.render("all-shoes", { articles });
-    })
-    .catch((err) => {
-      console.error(err);
-      res.send("Verileri çekme sırasında hata oluştu.");
-    });
-});
-
 app.get("/user", (req, res) => {
   if (req.session.user) {
     res.render("user", { user: req.session.user });
@@ -115,13 +96,9 @@ app.get("/user", (req, res) => {
 });
 
 app.post("/user", (req, res) => {
+  // Handle the creation and saving of user data (Contact model).
   const { title, summary, number, shoesname, body } = req.body;
-
   const contact = new Contact({ title, summary, number, shoesname, body });
-
-  // veri nesnesi oluşturma
-
-  // veri nesnesinin veritabanına kaydedilmesi
 
   contact
     .save()
@@ -130,7 +107,7 @@ app.post("/user", (req, res) => {
     })
     .catch((err) => {
       console.error(err);
-      res.send("Veritabanına kaydetme sırasında hata oluştu.");
+      res.send("An error occurred while saving to the database.");
     });
 });
 
@@ -139,45 +116,38 @@ app.get("/signup", (req, res) => {
   res.render("signup");
 });
 
+// Signup Route
 app.post("/signup", (req, res) => {
+  // Handle user registration and nodemailer email sending here.
   const { username, email, password } = req.body;
 
-  User.create({ username, email, password })
-    .then((user) => {
-      req.session.user = user;
+  // Create a transporter
+  const transporter = nodemailer.createTransport({
+    service: "Gmail",
+    auth: {
+      user: "ammaryasir8088@gmail.com",
+      pass: "afbp vpqe bfsv bpvi",
+    },
+  });
 
-      // Create a transporter using your email provider's SMTP settings
-      const transporter = nodemailer.createTransport({
-        service: "Gmail",
-        auth: {
-          user: "ammaryasir8088@gmail.com",
-          pass: "tqpzjkrepkehhqhu",
-        },
-      });
+  // Email content
+  const mailOptions = {
+    from: "ammaryasir8088@gmail.com",
+    to: email,
+    subject: "Welcome",
+    text: `Hello ${username}, Your registration was successful.`,
+  };
 
-      // Compose the email message
-      const mailOptions = {
-        from: "signup-login-project@gmail.com",
-        to: user.email, // Use the user's email address here
-        subject: "Welcome to Your Website",
-        text: "Thank you for signing up.your password is " + user.username,
-      };
-
-      // Send the email
-      transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-          console.log(error);
-        } else {
-          console.log("Email sent: " + info.response);
-        }
-      });
-
-      res.redirect("/user");
-    })
-    .catch((err) => {
-      console.error(err);
-      res.send("An error occurred while saving the user.");
-    });
+  // Send email
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      console.log(error);
+      res.send("Email sending error");
+    } else {
+      console.log("Email sent: " + info.response);
+      res.send("Registration completed. Email sent.");
+    }
+  });
 });
 
 // Login Route
@@ -194,12 +164,12 @@ app.post("/login", (req, res) => {
         req.session.user = user;
         res.redirect("/user");
       } else {
-        res.send("Hatalı e-posta veya şifre.");
+        res.send("Invalid email or password.");
       }
     })
     .catch((err) => {
       console.error(err);
-      res.send("Kullanıcı bulunamadı.");
+      res.send("User not found.");
     });
 });
 
@@ -214,13 +184,10 @@ app.get("/contactus", (req, res) => {
 });
 
 app.post("/contactus", (req, res) => {
-  // form verilerinin alınması
+  // Handle contact form data and saving to the database.
   const { title, summary, number, body } = req.body;
-
-  // veri nesnesi oluşturma
   const contact = new Contact({ title, summary, number, body });
 
-  // veri nesnesinin veritabanına kaydedilmesi
   contact
     .save()
     .then(() => {
@@ -228,12 +195,10 @@ app.post("/contactus", (req, res) => {
     })
     .catch((err) => {
       console.error(err);
-      alert("An error occurred while saving to the database."); // An alert message is added
-      res.redirect("/contactus"); // Redirecting to "/contactus" page
+      res.send("An error occurred while saving to the database.");
     });
 });
 
-// 404 Route
 app.use((req, res) => {
   res.status(404).render("404");
 });
